60214a8ca68f760fa3ea36e7a6ddbb2fd128a3ea
Matthys <matthysk@gmail.com>
2021-01-06 18:11:39 +0200

    Add function to load parameters from file

---
+++ R/load_parameters.R
@@ -0,0 +1,39 @@
+
+#' Loads simulation parameters from a file
+#'
+#' This function reads parameters from a file and reformats them so they are ready to be used in the model
+#'
+#' @param file_path String The path to the file to be loaded
+#' @param sim_name String Optional The name of the simulation to be extracted from the file
+#' @param start_date Date Must be provided if the file contains start_date instead of start_time.
+#'   It is the date that will be considered time zero.
+#'
+#'@export
+
+load_parameters <- function(file_path, sim_name = NULL, start_date = NULL) {
+  data <- readr::read_delim(file=file_path,
+                            delim=";",
+                            col_types=readr::cols()
+                            ) %>%
+    dplyr::mutate(start_date = lubridate::dmy(start_date))
+
+  if (!is.null(sim_name)){
+    data <- data %>%
+      dplyr::filter(sim_name == {{sim_name}})
+  }
+
+  if (!is.null(start_date)){
+    data <- data %>%
+    dplyr::mutate(start_time = ifelse(is.na(start_date), 0, start_date - {{start_date}}))
+  } else {
+    stopifnot(start_time %in% data)
+  }
+
+  data <- data %>%
+    dplyr::arrange(sim_name, parameter_name, start_time) %>%
+    dplyr::group_by(parameter_name) %>%
+    dplyr::mutate(end_time = dplyr::lead(start_time)) %>%
+    dplyr::ungroup()
+
+  return(data)
+}
---
+++ tests/testthat/test-load_parameters.R
@@ -0,0 +1,17 @@
+
+
+test_that("loading parameters works", {
+
+  expected_columns <- c("sim_name", "parameter_name", "value", "start_time", "end_time")
+
+  file_path <- system.file("test-data", "manually_edited_parameters.csv", package="conisi")
+  parameters <- conisi::load_parameters(file_path, sim_name = "sa_37", start_date = lubridate::ymd("2020-03-10"))
+
+  matching_cols <- intersect(expected_columns, names(parameters))
+  expect_equal(expected_columns, matching_cols)
+  expect_equal(nrow(parameters), 48)
+
+  foi_adjust <- dplyr::filter(parameters, parameter_name == "FOIadjust")
+
+  expect_equal(foi_adjust$end_time[1], foi_adjust$start_time[2])
+})
