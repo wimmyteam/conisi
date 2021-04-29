58a30ae29135913656d24fed2df2dc73f15e9437
Matthys Kroon <matthys.kroon@wimmy.co.za>
2021-04-29 13:47:10 -0500

    Improve input validation in load_parameters

--- R/load_parameters.R
+++ R/load_parameters.R
@@ -17,7 +17,11 @@ load_parameters <- function(file_path, sim_name = NULL, start_date = NULL) {
                             ) %>%
     dplyr::mutate(start_date = lubridate::dmy(start_date))
 
+
+
   if (!is.null(sim_name)){
+    if(!"sim_name" %in% colnames(data))
+      {stop("Trying to filter on sim_name but parameter file does not have sim_name column")}
     data <- data %>%
       dplyr::filter(sim_name == {{sim_name}})
   }
@@ -26,7 +30,7 @@ load_parameters <- function(file_path, sim_name = NULL, start_date = NULL) {
     data <- data %>%
     dplyr::mutate(start_time = ifelse(is.na(start_date), 0, start_date - {{start_date}}))
   } else {
-    stopifnot("start_time" %in% data)
+    stopifnot("start_time" %in% colnames(data))
   }
 
   data <- data %>%
