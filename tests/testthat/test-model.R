1dbcf57186902719d53dd35675bf20c5ea7bd6ed
Emanuel Dominic <emanuel@aims.ac.za>
2021-06-10 11:50:02 +0200

    updated model tests

--- tests/testthat/test-model.R
+++ tests/testthat/test-model.R
@@ -12,10 +12,13 @@ test_that("Model helper functions work", {
   par_table <- read.csv(par_file, sep=";")
 
   pop <- 1000000
-  mod_result <- COVIDmodel_run_and_mutate(par_table, pop, 100)
+  pop_prop <- c(0.03,0.39,0.58)
+  contact_matrix <- c(13.6, 10.69, 15.71,0.80, 4, 7.2, 0.80, 4.90, 4)
+
+  mod_result <- COVIDmodel_run_and_mutate(par_table, pop, 100, pop_prop, contact_matrix)
   #mod_result <- dplyr::mutate(mod_result, experiment = 1)
 
-  expect_length(mod_result, 251)
+  expect_length(mod_result, 875)
 
   availablecores <- 2
   doParallel::registerDoParallel(cores = availablecores)
@@ -25,9 +28,9 @@ test_that("Model helper functions work", {
       dplyr::mutate(par_table, experiment = 2)
       )
 
-  mod_result_2 <- COVIDmodel_run_many(xparm_table, pop, 100)
-  expect_length(mod_result_2, 70)
+  mod_result_2 <- COVIDmodel_run_many(xparm_table, pop, 100, pop_prop, contact_matrix)
+  expect_length(mod_result_2, 118)
 
-  mod_result_3 <- COVIDmodel_run_and_mutate_many(xparm_table, pop, 100)
-  expect_length(mod_result, 251)
+  mod_result_3 <- COVIDmodel_run_and_mutate_many(xparm_table, pop, 100, pop_prop, contact_matrix)
+  expect_length(mod_result3, 875)
 })
